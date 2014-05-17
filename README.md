ca7ecfe37cc4454248d346f614a83915db0e6380
Scott Robinett <srobin04005@gmail.com>
2014-05-17 15:31:15 -0400

    Getting and Cleaning Data

---
+++ README.md
@@ -0,0 +1,42 @@
+==================================================================
+Average Mean and Standard Deviation by subject and activity of Human Activity Recognition Using Smartphones Dataset
+Version 1.0
+==================================================================
+Ronald Robinett
+Getting and Cleaning Data
+Johns Hopkins University
+Coursera MOOC - Data Science
+srobin04005@gmail.com
+==================================================================
+
+This dataset was derived from experiments that were carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. 
+
+The result set (tidy.txt) for this analysis represents only the mean and standard deviation observations aggregated for each activity, by subject. 
+
+The dataset includes the following files:
+=========================================
+
+- 'README.md': This file
+
+- 'COOKBOOK.md' :  Description of process and variables 
+
+- 'run_analysis.R':  R source file that is used to obtain data and process datasets
+
+- 'tidy.txt':  tidy dataset of merged data from test.txt and train.txt, averaged as an aggregate for each subject and activity.
+
+Notes: 
+======
+- 'train/Inertial Signals' files from the original study were not used in this analysis 
+- 'test and train' data were merged to create results
+
+For more information about the Human Activity Recognition Using Smartphones Dataset contact: activityrecognition@smartlab.ws
+
+License:
+========
+Use of this Human Activity Recognition Using Smartphones Dataset in publications must be acknowledged by referencing the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
---
+++ run_analysis.R
@@ -0,0 +1,99 @@
+# setup working directory 
+setwd("C:\\Users\\srobin\\Documents\\R\\data\\GettingCleaning")
+
+# Document date, file and zip file create
+downloadDate <- date()
+file <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+myZip <- "UCI HAR Dataset.zip"
+
+#download Datasets zip file
+download.file(file, destfile = myZip)
+
+# load the utils library and unzip the downloaded file into getwd()
+library(utils)
+unzip(myZip)
+
+# set directory and load the activity and features data
+setwd("C:\\Users\\srobin\\Documents\\R\\data\\GettingCleaning\\UCI HAR Dataset")
+act_desc <- read.table("activity_labels.txt")
+features <- read.table("features.txt")
+
+testdir <- "C:\\Users\\srobin\\Documents\\R\\data\\GettingCleaning\\UCI HAR Dataset\\test"
+traindir <- "C:\\Users\\srobin\\Documents\\R\\data\\GettingCleaning\\UCI HAR Dataset\\train"
+
+# dir and dataset parameters
+processSets <- function(dir,dataset){
+
+  # set working directory for the test dataset 
+  # read the datasets into R (train or test)
+  # process X_ data 
+	# load X_data 
+	# apply features names to the columns
+	# Extract mean and std columns
+	# add an ID column to each subset
+	# merge the two subsets as dataX
+  setwd(dir)
+  dataX <- read.table(paste0("X_",dataset,".txt"))
+  feaNames <- features[,2]
+    names(dataX) <- feaNames
+    testmean <- dataX[,grep("(?=.*mean*)",names(dataX),perl=T)]
+    testmean$ID <- 1:nrow(testmean)
+    teststd  <- dataX[,grep("(?=.*std*)",names(dataX),perl=T)]
+    teststd$ID <- 1:nrow(teststd)
+    dataX <- merge(testmean, teststd,by="ID")
+
+  # load y_data and subject_data
+  datay <- read.table(paste0("y_",dataset,".txt"))
+  datasubject <- read.table(paste0("subject_",dataset,".txt"))
+  
+  # add keyID as a row index for each data file
+  dataX$keyID <- 1:nrow(dataX)
+  datay$keyID <- 1:nrow(datay)
+  datasubject$keyID <- 1:nrow(datasubject)
+
+  # change the names activity and subject
+  names(datay) [1] <- "activity"
+  names(datasubject) [1] <- "subject"
+  
+  # load library to use arrange
+  library(plyr)
+  # join train datasets by keyID
+  data_y_subject <- arrange(join(datay,datasubject),keyID)
+  output <- arrange(join(dataX,data_y_subject), keyID)
+      
+  return(output)
+}
+
+# get test and train datasets
+test <- processSets(testdir,'test')
+train <- processSets(traindir,'train')
+
+# merge test and train data - more like, append train to test
+# basically, stacking one on top of the other
+mergeData = rbind(test,train)
+
+# Use descriptive activity names
+# quick and dirty :(
+mergeData$activity[mergeData$activity == 1] <- "WALKING"
+mergeData$activity[mergeData$activity == 2] <- "WALKING_UPSTAIRS"
+mergeData$activity[mergeData$activity == 3] <- "WALKING_DOWNSTAIRS"
+mergeData$activity[mergeData$activity == 4] <- "SITTING"
+mergeData$activity[mergeData$activity == 5] <- "STANDING"
+mergeData$activity[mergeData$activity == 6] <- "LAYING"
+
+
+# remove ID columns
+mergeData$ID <- NULL
+mergeData$keyID <- NULL
+
+# Create second, independent tidy dataset with the average 
+# of each variable for each activity and each subject
+# label dataset with descriptive activity names
+
+# melt dataset using last two columns (subject, activity) as id.vars
+meltdata <- melt(mergeData, id=80:81)
+# cast the meltdata using subjec and activity to group on
+# apply average (mean) to all variable.
+myCast <- dcast(meltdata, subject + activity ~ variable, mean)
+names(myCast)[3:81] <- paste0("avg-",names(myCast)[3:81])
+
